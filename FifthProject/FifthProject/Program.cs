974409a73ff42413460ffb6697a2f6276da6225a
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-04 17:54:40 +0200

    Third Day - arrays

---
+++ FifthProject/FifthProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+
+namespace FifthProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Nissan" };
+
+            Console.WriteLine(cars[0]);
+            int arrayLength = cars.Length; // 3
+            cars[2] = "Tesla";
+
+            Console.WriteLine(cars[arrayLength - 1]);
+        }
+    }
+}
