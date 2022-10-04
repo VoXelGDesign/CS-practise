6f78601769308fe506c257410459bba31494e9cc
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-04 17:40:34 +0200

    Third Day - Conversion and Projection

---
+++ FourthProject/FourthProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+
+namespace FourthProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte bValue = 100;
+            int iValue = bValue;
+            byte bValue2 = (byte)iValue;
+            double dValue = 3.5;
+            int iValue2 = (int)dValue;
+
+            string sValue2 = iValue2.ToString();
+
+            string userInput = Console.ReadLine();
+            int yearOfBirth;
+
+            if(int.TryParse(userInput, out yearOfBirth))
+            {
+                int age = DateTime.Now.Year - yearOfBirth;
+
+                Console.WriteLine("Your Age: " + age);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Value");
+            }
+           
+
+        }
+    }
+}
