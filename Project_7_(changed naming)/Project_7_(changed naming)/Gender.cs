e7189ad18c987ad8b12b9bed6822109182bac696
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-07 00:33:54 +0200

    Comeback after fight with malware . Next day and starting with enum!

---
+++ Project_7_(changed naming)/Project_7_(changed naming)/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Project_7
+{
+   enum Gender : byte
+    {
+        Male = 1,
+        Female = 2
+    }
+
+}
---
+++ Project_7_(changed naming)/Project_7_(changed naming)/Program.cs
@@ -0,0 +1,27 @@
+﻿using Project_7;
+using System;
+using System.Text;
+
+
+namespace Prjoect_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your gender?   1 - Male, 2 - Female");
+            string inp = Console.ReadLine();
+
+            Gender userGender = (Gender)Enum.Parse(typeof(Gender), inp);
+
+            if(userGender == Gender.Male)
+            {
+                Console.WriteLine("Male was selected");
+            }
+            else
+            {
+                Console.WriteLine("Female was selected");
+            }
+        }
+    }
+}
