27635cf5c646fa16bb7018397179c5fcde7b9ea5
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-07 01:05:31 +0200

    Use of namespaces for organizing code

---
+++ Prjoect_10/Prjoect_10/Enums/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prjoect_10.Enums
+{
+    enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ Prjoect_10/Prjoect_10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using Prjoect_10.Enums;
+namespace Project_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gender gender = Gender.Male;
+            Console.WriteLine(gender);
+        }
+    }
+}
