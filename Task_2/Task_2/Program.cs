6c3021e5bd1e7d497604f4c13a1d549bd2bf98e6
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-07 01:58:58 +0200

    Task 2 completed !

---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+
+namespace Task_2
+{
+    class Program
+    {
+        
+        /// Task two - write program that calculate days elapsed 
+        /// since entered date
+        
+        static void Main(string[] args)
+        {
+            
+            
+           Console.WriteLine("Enter your date of birth :");
+           DateTime date;
+           if (!DateTime.TryParse(Console.ReadLine(), out date))
+           {
+                Console.WriteLine("Invalid Date!");
+           }
+           else
+           {
+                Console.WriteLine("Days Elapsed: " + ((int)DateTime.Now.Subtract(date).TotalDays));
+           }
+                
+        }
+    }
+}
