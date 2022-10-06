7d9bb0f7702b552a37a771ee383b19533de56a06
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-07 00:56:53 +0200

    Try-Catch-Finally - handling exceptions

---
+++ Project_9/Project_9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Project_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Mazda" };
+
+
+            try
+            {
+                cars[4] = "Tesla"; // throws exception 
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Index out of range!");
+                // executed when exception "IndexOutOfRangeException" is caught
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown exception :(");
+            }
+            finally
+            {
+                Console.WriteLine("Some cleanup");
+                // Always executed even with exepction
+            }
+
+            Console.WriteLine("Try-Catch ended!");
+        }
+    }
+}
