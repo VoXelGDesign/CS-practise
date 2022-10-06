dd2bf41585333f9fbfe4c47324ee357ea89c1d10
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-07 00:43:23 +0200

    Nullable types :)

---
+++ Project_8/Project_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+
+namespace Prjoect_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? favNum = null; // int or Nullable<int>
+            
+            Console.WriteLine("Favourite number: " + (favNum.HasValue ? favNum.Value.ToString() : ""));
+        }
+    }
+}
