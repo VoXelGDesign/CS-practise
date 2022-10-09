832b542e6cfb1dc5054dc4fd1e567a33ec85072e
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-09 22:25:32 +0200

    Regex - email verification

---
+++ Project_11/Project_11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Regex regex = new Regex(@"^([a-z0-9]+)\.?([a-z0-9]+)@([a-z]+)\.[a-z]{2,3}$");
+        string testEmail = "test23.12@gmail.com";
+        Console.WriteLine(regex.IsMatch(testEmail));
+
+    }
+}
