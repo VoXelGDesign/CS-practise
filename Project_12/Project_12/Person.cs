5cc3ea60e8212bc320d553a57026e7fbc3072c0d
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-09 23:01:04 +0200

    OOP - fields, methods, constructors

---
+++ Project_12/Project_12/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_12
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        private DateTime dateOfBirth;
+
+        public Person(string firstName, string lastName)
+        {
+            Console.WriteLine("Construktor1");
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Person(DateTime dateOfBirth,string firstName, string lastName) : this(firstName, lastName)
+        {
+            Console.WriteLine("Construktor2");
+            SetDateOfBirth(dateOfBirth);
+        }
+        public void SetDateOfBirth(DateTime date)
+        {
+            if (date < DateTime.Now) dateOfBirth = date;
+            else Console.WriteLine("Invalid date of Birth!");
+        }
+
+        public DateTime GetDateOfBirth() => dateOfBirth; // or older version public DateTime GetDateOfBirth(){ return dateOfBirth;}
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {this.FirstName}, {this.GetDateOfBirth()}");
+        }
+    }
+}
---
+++ Project_12/Project_12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Project_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bill = new Person("Bill","Wick");
+            bill.SetDateOfBirth(new DateTime(1990,1,1));          
+            bill.SayHi();
+
+            Person john = new Person(new DateTime(1990,1,2),"John","Wick");
+
+            john.SayHi();
+        }
+    }
+}
