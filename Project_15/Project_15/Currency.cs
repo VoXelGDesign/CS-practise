11470df7d6721df5ea05f056e9ddf0c08cb960bd
Jakub Nowakowski <trynewgame@gmail.com>
2022-10-15 19:57:05 +0200

    Dictionary

---
+++ Project_15/Project_15/Currency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency
+{
+    internal class Currency
+    {
+        public string currencyCode { get; set; }
+        public string currencyName { get; set; }
+        public double Rate { get; set; }
+        public Currency(string code, string name, double rate)
+        {
+            currencyCode = code;
+            currencyName = name;
+            Rate = rate;
+        }
+        
+    }
+}
---
+++ Project_15/Project_15/Program.cs
@@ -0,0 +1,34 @@
+﻿using currency;
+using System.Linq;
+internal class Program
+{
+    static Dictionary<string,Currency> GetCurrencies()
+    {
+        return new Dictionary<string, Currency>
+        {
+            {"USD",new Currency("USD","United States Dollar",1)},
+            {"EUR",new Currency("EUR","Euro",0.83975) },
+            {"GBP", new Currency("GBP","British Pound",0.74771) },
+            {"CAD", new Currency("CAD","Canadian Dollar",1.30724) },
+            {"INR", new Currency("INR","Indian Rupee", 73.04) },
+            {"MXN", new Currency("MXN","Mexican Peso",21.7571) },
+
+        };
+    }
+    private static void Main(string[] args)
+    {
+        Dictionary<string,Currency> currencies = GetCurrencies();
+        Console.WriteLine("Check the rate for");
+        string userInput = Console.ReadLine();
+
+        Currency selectedCurrency = null;
+        if(currencies.TryGetValue(userInput, out selectedCurrency))
+        {
+            Console.WriteLine($"Current rate for USD to {selectedCurrency.currencyName} is {selectedCurrency.Rate}");
+        }
+        else
+        {
+            Console.WriteLine("Code doesn't match");
+        }
+    }
+}
