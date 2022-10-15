e08176012b05e528ae89a58632f84ba0a4631861
Jakub Nowakowski <jakunow552@student.polsl.pl>
2022-10-15 16:49:22 +0200

    Preparation for LINQ

---
+++ Project_14/Project_14/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_14
+{
+    internal class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        private DateTime dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get => dateOfBirth;
+            set { dateOfBirth = value; }
+        }
+
+        private string? phoneNumber;
+        
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            set
+            {
+                if (value.Length == 9) phoneNumber = value;
+                else Console.WriteLine("Invalid number!");
+            }
+        }
+
+        public static int Count = 0;
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.dateOfBirth = dateOfBirth;
+            Count++;
+        }
+        public Person(string fName, string lName, DateTime dOfBirth, string phoneNumber) : this(fName, lName, dOfBirth)
+        {           
+            this.phoneNumber = phoneNumber;
+        }
+
+        
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi I'm {this.firstName} {this.lastName}, {this.dateOfBirth}, Number: {this.phoneNumber}");
+        }
+
+    }
+}
---
+++ Project_14/Project_14/Program.cs
@@ -0,0 +1,42 @@
+﻿using Project_14;
+internal class Program
+{
+    static List<Person> GetEmployees()
+    {
+        List<Person> employees = new List<Person>
+            {
+                new Person("John","Wick",new DateTime(2000,4,7),"111222333"),
+                new Person("Bill","Wick",new DateTime(1999,7,7),"012345678"),
+                new Person("Mark","Smith",new DateTime(2002,9,1),"789456123"),
+                new Person("Bob","Dickens",new DateTime(2003,11,20)),
+                new Person("Andrew","Tate",new DateTime(1993,10,11),"111666111"),
+                new Person("Ed","Sheeran",new DateTime(2005,8,27),"890765345"),
+                new Person("Bill","Wook",new DateTime(1978,3,14),"767555783")
+            };
+
+        return employees;
+    }
+    private static void Main(string[] args)
+    {
+        List<Person> employees = GetEmployees();
+        List<Person> youngEmployees = new List<Person>();
+        foreach (Person employee in employees)
+        {
+            if(employee.DateOfBirth > new DateTime(2000, 1, 1))
+            {
+                youngEmployees.Add(employee);
+            }
+            
+        }
+        Console.WriteLine($"Young employees count: {youngEmployees.Count}");
+
+        
+        foreach(Person youngEmployee in youngEmployees)
+        {
+            if(youngEmployee.firstName == "Bob")
+            {
+                youngEmployee.SayHi();
+            }
+        }
+    }
+}
